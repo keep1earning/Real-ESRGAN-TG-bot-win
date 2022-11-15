4a1aed2eec9dbb64ec1829356cbf2221030c5df9
sakurajima-mai <mowilliam416@gmail.com>
2022-11-16 01:52:50 +0800

    Update install_realesrgan-ncnn-vulkan.ps1

--- install_realesrgan-ncnn-vulkan.ps1
+++ install_realesrgan-ncnn-vulkan.ps1
@@ -1,11 +1,8 @@
-Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
-irm https://raw.githubusercontent.com/scoopinstaller/install/master/install.ps1 | iex
-scoop install unzip wget
-wget.exe https://github.com/xinntao/Real-ESRGAN-ncnn-vulkan/releases/download/v0.2.0/realesrgan-ncnn-vulkan-v0.2.0-windows.zip
-unzip realesrgan-ncnn-vulkan-v0.2.0-windows.zip
-mv .\realesrgan-ncnn-vulkan-v0.2.0-windows\realesrgan-ncnn-vulkan.exe .\realesrgan-ncnn-vulkan.exe
-mv .\realesrgan-ncnn-vulkan-v0.2.0-windows\vcomp140.dll .\vcomp140.dll
-mv .\realesrgan-ncnn-vulkan-v0.2.0-windows\vcomp140d.dll .\vcomp140d.dll
-rm .\realesrgan-ncnn-vulkan-v0.2.0-windows -r
-rm .\realesrgan-ncnn-vulkan-v0.2.0-windows.zip
+wget.exe https://github.com/xinntao/Real-ESRGAN/releases/download/v0.2.5.0/realesrgan-ncnn-vulkan-20220424-windows.zip
+unzip realesrgan-ncnn-vulkan-20220424-windows.zip
+rm .\input.jpg
+rm .\input2.jpg
+rm .\README_windows.md
+rm .\onepiece_demo.mp4
+rm .\realesrgan-ncnn-vulkan-20220424-windows.zip
 echo "Finished"
