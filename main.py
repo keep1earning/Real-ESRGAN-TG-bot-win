e8ef39d8cc8a542bb80f22b9b4ef1221f3f6cceb
sakurajima-mai <mowilliam416@gmail.com>
2022-11-16 01:55:56 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import json
 from retrying import retry
 from telethon import TelegramClient, events
 from tg_bot_config import api_hash, api_id, bot_token
-model_list = ["realesr-animevideov3","realesrgan-x4plus","realesrgan-x4plus-anime","realesrnet-x4plus"]
+model_list = ["realesr-animevideov3","realesrgan-x4plus","realesrgan-x4plus-anime"]
 models = "realesr-animevideov3"
 client = TelegramClient('bot', api_id, api_hash).start(bot_token=bot_token)
 @client.on(events.NewMessage(incoming=True))
@@ -37,7 +37,7 @@ async def my_event_handler(event):
             os.system("pwsh -Command rm .\*.webp")
             print("Cache cleaned")
         if event.raw_text == "/ava_model":
-            await event.reply("目前的模型是 " + models + " , 可用模型: realesr-animevideov3 | realesrgan-x4plus | realesrgan-x4plus-anime | realesrnet-x4plus")
+            await event.reply("目前的模型是 " + models + " , 可用模型: realesr-animevideov3 | realesrgan-x4plus | realesrgan-x4plus-anime ")
         if event.raw_text[:6] == "/model":
             if event.raw_text[7:] in model_list:
                 models = event.raw_text[7:]
