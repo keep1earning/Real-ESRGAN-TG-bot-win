57601a631590e04d4aed862d5ba26f0f7e8413a9
sakurajima-mai <mowilliam416@gmail.com>
2022-11-16 23:10:11 +0800

    Update install_realesrgan-ncnn-vulkan.ps1

--- install_realesrgan-ncnn-vulkan.ps1
+++ install_realesrgan-ncnn-vulkan.ps1
@@ -1,3 +1,5 @@
+Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
+irm get.scoop.sh | iex
 wget.exe https://github.com/xinntao/Real-ESRGAN/releases/download/v0.2.5.0/realesrgan-ncnn-vulkan-20220424-windows.zip
 unzip realesrgan-ncnn-vulkan-20220424-windows.zip
 rm .\input.jpg
